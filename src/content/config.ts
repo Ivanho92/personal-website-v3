27a03de47c902a9cb9b4b4f3fd2251cd6439796c
Ivan Rodrigues <iro@efficy.com>
2023-12-02 17:12:20 +0100

    init commit

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const projectsCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    "live-url": z.string().url().optional(),
+    "code-url": z.string().url().optional(),
+    image: image(),
+    summary: z.string().optional(),
+    concepts: z.array(z.string()).optional(),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'projects': projectsCollection,
+};
---
+++ src/content/projects/tic-tac-toe.md
@@ -0,0 +1,12 @@
+---
+title: Tic-Tac-Toe
+tags: 
+  - 'javascript'
+  - 'css'
+live-url: https://ir-js-tictactoe.netlify.app/
+code-url: https://github.com/Ivanho92/vanilla-js-tic-tac-toe
+image: ./images/tictactoe.png
+summary: 'The popular Tic-Tac-Toe game made with pure Vanilla JavaScript. Try it with a friend🎮'
+concepts:
+  - Vanilla HTML/CSS/JavaScript
+---
